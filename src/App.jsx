a20bee450ed04bd0d99cbc36ac9a7e14281df282
William david <Willicali40@hotmail.com>
2023-04-08 18:03:11 -0500

    application needs to be optimise for mobile and needs to be redirect to octocat user when user not found

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { Container } from "@mui/material";
+
+import Searcher from "./components/Searcher/Searcher";
+
+import { getGitHubUser } from './services/users'
+import UserCard from "./containers/UserCard/index";
+
+const App = () => {
+
+  const [inputUser, setInputUser] = useState('octocat');
+  const [userState, setUserState] = useState('inputUser');
+  const [notFound, setNotFound] = useState(false);
+
+  const gettinUser = async (user) => {
+    const userResponse = await getGitHubUser(user)
+
+    if(userState === 'octocat'){
+      localStorage.setItem('octocat', userResponse)
+    }
+
+    if(userResponse.message === 'Not Found'){
+      const { octocat } = localStorage;
+      setInputUser(octocat);
+      setNotFound(true);
+    } else {
+      setUserState(userResponse);
+    }
+
+  };
+
+  console.log(userState);
+
+  useEffect(() =>{
+    gettinUser(inputUser)
+  },
+  [inputUser])
+
+  return(
+    <Container sx={{
+      background: 'whitesmoke',
+      width: '80vw',
+      height: '500px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }} >
+      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+      <UserCard userState={userState}/>
+    </Container>
+  )
+};
+
+export default App;
---
+++ src/containers/Description/index.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import { Stack, Typography } from "@mui/material";
+
+import PaperInformation from "../../components/PaperInformation/index";
+import LocationInformation from "../../components/LocationInformation/index";
+
+const Description = (props) => {
+  const { userState } = props;
+  const { bio } = userState
+  return(
+    <Fragment>
+      <Stack sx={{justifyContent: 'center'}}>
+        {bio !== null 
+          ? <Typography variant='body1'>{bio}</Typography>
+          : <Typography variant="body1">Lorem ipsum dolor sit amet. Ea excepturi doloribus ea praesentium nihil nam dolore laboriosam ad fugit nobis</Typography>
+        }
+      </Stack>
+      <PaperInformation userState={userState} />
+      <LocationInformation userState={userState} />
+    </Fragment>
+  )
+};
+
+export default Description;
